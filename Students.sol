d8cb57156c8b3f9e1e8cf354a7f8c031a45ee4bc
Strucker.eth <93291951+TheScotlandyard@users.noreply.github.com>
2022-10-08 15:18:23 -0400

    Update Students.sol
    A Smart Contract for registering Student Details.

--- Students.sol
+++ Students.sol
@@ -1,2 +1,18 @@
 //SPDX-License-Identifier:MIT
 pragma solidity ^0.8.0;
+
+contract StudentRegistration {
+    constructor(){
+        address studentID;
+        address public owner;
+        let students = map(address => studentID);
+        require(msg.sender = owner, "You lack ownership privileges!");
+    }
+ 
+    modifier onlyOwner(adress){
+        require(msg.sender = owner, "You lack ownership privileges!");
+        _;
+    }
+
+
+}
