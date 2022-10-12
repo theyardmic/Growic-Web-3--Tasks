a7362eff76ae9f56878d298f9178ccfc4ac920f7
Strucker.eth <93291951+strucker-eth@users.noreply.github.com>
2022-10-12 03:14:31 -0400

    Update User-balance.sol

--- User-balance.sol
+++ User-balance.sol
@@ -9,13 +9,13 @@ address accountOwner = msg.sender;
 
     //deposit (uint256 amount) this function accepts one argument and it saves the amount a user is depositing into a mapping,
 function deposit(uint256 amount) public view {
-    amount = _amount;
-    mapping(address => uint256) myMap;
+    uint256 amount = _amount;
+    mapping(address => uint256) public userBalance;
 }
 
 //checkBalance() this function searches for the user balance inside the balance mapping and returns the balance of whoever is calling the contract.
 function checkBalance(address _address, uint256 _amount) public view returns(address, uint256) {
-    return myMap[accountOwner];
+    return UserBalance[accountOwner];
 }
 
 
