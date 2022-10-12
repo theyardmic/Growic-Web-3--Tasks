aab12db4288d3c2dfa450b19b5f6589dfd223365
Strucker.eth <93291951+strucker-eth@users.noreply.github.com>
2022-10-12 05:15:12 -0400

    Update Add KYC Info.sol

--- Add KYC Info.sol
+++ Add KYC Info.sol
@@ -1,30 +1,19 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
 contract UserDetails {
+         //defined struct
     struct Details{
      string name;
      uint256 age;
     }
+         Details[] public details;
+    //this function accepts 2 arguments that represent the details of the user calling the smart contract and it saves them into a defined struct,
+ function setUserDetails(string calldata name, uint256 age) external {
+        string name = _name;
+        uint256 age = _age;
 
-//defined struct
-    Details[] public details;
-
-//this function accepts 2 arguments that represent the details of the user calling the smart contract and it saves them into a defined struct,
-function setUserDetails(string calldata name, uint256 age) external {
-string name = _name;
-uint256 age = _age;
-
- details.push(Details({name: _name, age = _age}));
-
-// todos.push(Todo(_text, false));
- details.push(Details({name: _name, age = _age}));
- //todos.push(Todo({text: _text, completed: false}));
-
+     details.push(Details({name: _name, age: _age}));
+    }
 }
-
-}
-
-
 
 }
