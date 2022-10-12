e94ede82b28e210eb3d91b8a2701f0fbb1284ade
Strucker.eth <93291951+strucker-eth@users.noreply.github.com>
2022-10-12 06:16:37 -0400

    Update Add KYC Info.sol

--- Add KYC Info.sol
+++ Add KYC Info.sol
@@ -12,10 +12,7 @@ struct Details {
 
     //this function accepts 2 arguments that represent the details of the user calling the smart contract and it saves them into a defined struct,
  function setUserDetails(string calldata name, uint256 age) public view {
-       string name = _name;
-       uint256 age = _age;
-
-        _details.push(Details({name: _name, age: _age}));    
+             _details.push(Details({name: _name, age: _age}));    
     }
 
 //getUserDetail() this function retrieves and returns the details saved for the user calling the contract.
