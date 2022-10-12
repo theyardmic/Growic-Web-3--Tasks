620278c24952503676e36548dc62fccd755ed430
Strucker.eth <93291951+strucker-eth@users.noreply.github.com>
2022-10-12 03:00:12 -0400

    Update User-balance.sol

--- User-balance.sol
+++ User-balance.sol
@@ -1,7 +1,12 @@
 //SPDX-License-Identifier:MIT
 pragma solidity ^0.8.0;
 
-contract Balance{
+contract Balance {
+
+constructor() public {
+address accountOwner = msg.sender;
+}
+
     //deposit (uint256 amount) this function accepts one argument and it saves the amount a user is depositing into a mapping,
 function deposit(uint256 amount) public view {
     amount = _amount;
@@ -10,7 +15,7 @@ function deposit(uint256 amount) public view {
 
 //checkBalance() this function searches for the user balance inside the balance mapping and returns the balance of whoever is calling the contract.
 function checkBalance(address _address, uint256 _amount) public view returns(address, uint256) {
-    return myMap[msg.sender];
+    return myMap[accountOwner];
 }
 
 
