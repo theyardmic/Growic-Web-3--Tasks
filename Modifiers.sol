1a24d7e4ada5a2490716d7b4fbd9aa0c88ea1163
Strucker.eth <93291951+strucker-eth@users.noreply.github.com>
2022-10-13 03:20:51 -0400

    Update Modifiers.sol

--- Modifiers.sol
+++ Modifiers.sol
@@ -1,16 +1,17 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.7;
 
 contract Funds{
 
-constructor(address) {
-    owner = msg.sender;
-}
+constructor() public {
+   address owner = msg.sender;
+   uint _amount;
+   address _address;
+   }
 
 // Create a deposit function that allows anybody to send funds.
-function deposit(address _address, uint amount) public payable{
-    address _address = msg.sender;
-mapping(address => amount) public depositAmount;
+function deposit(address, uint) public payable {
+       mapping(address => uint) _amount;
 }
 
 //create a modifier that only allows the owner of the contract to withdraw the funds.
@@ -19,18 +20,26 @@ mapping(address => amount) public depositAmount;
             _;
     }
 
-//Add a withdraw function and 
+//Add a withdraw function and
     function withdrawFunds() public payable onlyOwner{
-        
-    }
 
-//Add an addFund function to  accept the amount to be added and update the mapping to have the new balance
+    }
 
 // create a modifier that only allows users that have deposited using the deposit function, to increase their balance on the mapping. 
 modifier onlyDeposited() {
-    require(amount !=0, "You have not deposited funds yet!" );
-    _;
+    if (_amount == 0) {
+        revert("You have not deposited funds yet!");
+    }
+       _;
 }
+
+//Add an addFund function to  accept the amount to be added and update the mapping to have the new balance
+    function addFund() public view onlyDeposited {
+        amount[_address] =  _amount[_address] + amount;
+
+    }
+
+
 
 
 }
