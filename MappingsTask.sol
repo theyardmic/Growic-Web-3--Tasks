56660c21f521f6d368dd2e5da5fe4c4e8b2074b5
Strucker.eth <93291951+strucker-eth@users.noreply.github.com>
2022-10-13 01:22:01 -0400

    Create MappingsTask.sol

---
+++ MappingsTask.sol
@@ -0,0 +1,29 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.0;
+
+contract Balance {
+
+    constructor(address) public {
+accountOwner = msg.sender;
+
+}
+   mapping(address => uint256) public balance;
+   
+    //deposit (uint256 amount) this function accepts one argument and it saves the amount a user is depositing into a mapping,
+function deposit(uint256 amount) public view {
+    uint256 amount = _amount;
+ 
+}
+
+    //checkBalance() this function searches for the user balance inside the balance mapping and returns the balance of whoever is calling the contract.
+function checkBalance() public external returns(uint256) {
+    uint256 userBalance = balance[msg.sender];
+    return userBalance;
+}
+
+
+}
+
+
+
+
