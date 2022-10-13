7378b62c03423f292e990285526078ca8bb5324e
Strucker.eth <93291951+strucker-eth@users.noreply.github.com>
2022-10-13 02:43:50 -0400

    Update Modifiers.sol

--- Modifiers.sol
+++ Modifiers.sol
@@ -1,2 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
+contract Funds{
+
+constructor(address) {
+    owner = msg.sender;
+}
+
+// Create a deposit function that allows anybody to send funds.
+function deposit(address _address, uint amount) public payable{
+    address _address = msg.sender;
+mapping(address => amount) public depositAmount;
+}
+
+}
