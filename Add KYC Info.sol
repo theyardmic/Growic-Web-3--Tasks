2bd0c9b879f53c306b0999ee516887f52c3d1c58
Strucker.eth <93291951+strucker-eth@users.noreply.github.com>
2022-10-23 19:03:34 +0300

    Update Add KYC Info.sol

--- Add KYC Info.sol
+++ Add KYC Info.sol
@@ -2,6 +2,30 @@
 pragma solidity ^0.8.7;
 
 contract UserDetails {
+        //previous task of mapping
+    constructor(address) public {
+accountOwner = msg.sender;
+
+}
+
+//mapping declaration
+   mapping(address => uint256) public balance;
+   
+    //deposit (uint256 amount) this function accepts one argument and it saves the amount a user is depositing into a mapping,
+function deposit(uint256 amount) public payable {
+ uint256 amount = msg.value;
+ balance[msg.sender += amount];
+ 
+}
+
+    //checkBalance() this function searches for the user balance inside the balance mapping and returns the balance of whoever is calling the contract.
+function checkBalance() public external returns(uint256) {
+    uint256 userBalance = balance[msg.sender];
+    return userBalance;
+}
+
+
+}
 
 struct Details {
     string  name;
