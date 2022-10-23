80f9a214c4920bd33ca4d92c887bb3c1636f6d3d
Strucker.eth <93291951+strucker-eth@users.noreply.github.com>
2022-10-23 19:20:03 +0300

    Update Modifiers.sol

--- Modifiers.sol
+++ Modifiers.sol
@@ -8,6 +8,27 @@ pragma solidity ^0.8.7;
 contract UserDetails {
  //previous task of structs
  
+ struct Details {
+    string  name;
+    uint256 age;
+}
+
+ Details[] public _details;
+
+    //this function accepts 2 arguments that represent the details of the user calling the smart contract and it saves them into a defined struct,
+ function setUserDetails(string calldata name, uint256 age) public view {
+             _details.push(Details({name: _name, age: _age}));    
+    }
+
+//getUserDetail() this function retrieves and returns the details saved for the user calling the contract.
+    function getuserDetail() external view {
+        return (_details[]);
+    }
+
+
+}
+
+}
         //previous task of mapping
     constructor(address) public {
 accountOwner = msg.sender;
