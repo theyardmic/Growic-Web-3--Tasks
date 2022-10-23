04558f82b5d96da8dd6866f9a8f72bb11ba7385a
Strucker.eth <93291951+strucker-eth@users.noreply.github.com>
2022-10-23 19:24:48 +0300

    Update Modifiers.sol

--- Modifiers.sol
+++ Modifiers.sol
@@ -1,40 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-contract Funds {
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
-
 contract UserDetails {
- //previous task of structs
- 
- struct Details {
-    string  name;
-    uint256 age;
-}
-
- Details[] public _details;
-
-    //this function accepts 2 arguments that represent the details of the user calling the smart contract and it saves them into a defined struct,
- function setUserDetails(string calldata name, uint256 age) public view {
-             _details.push(Details({name: _name, age: _age}));    
-    }
-
-//getUserDetail() this function retrieves and returns the details saved for the user calling the contract.
-    function getuserDetail() external view {
-        return (_details[]);
-    }
-
-
-}
-
-}
-        //previous task of mapping
-    constructor(address) public {
+  constructor(address) public {
 accountOwner = msg.sender;
 
 }
 
+ //previous task of structs and mappings
+    
 //mapping declaration
    mapping(address => uint256) public balance;
    
@@ -54,6 +28,8 @@ function checkBalance() public external returns(uint256) {
 
 }
 
+
+//struct declaration
 struct Details {
     string  name;
     uint256 age;
@@ -77,8 +53,9 @@ struct Details {
 }
     }
 
+         //modifier task
     uint256 public amount;
-    mapping(address => uint256) public balance;
+ 
 
 // Create a deposit function that allows anybody to send funds.
     function deposit(address, uint256) public payable {
@@ -86,6 +63,7 @@ struct Details {
        balance[msg.sender] += amount;
 
 }
+
 
 //create a modifier that only allows the owner of the contract to withdraw the funds.
     modifier onlyOwner(address) {
