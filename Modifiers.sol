df8e035ca579892b816a75570e85ea8fc1d61afc
Strucker.eth <93291951+strucker-eth@users.noreply.github.com>
2022-10-23 19:09:16 +0300

    Update Modifiers.sol

--- Modifiers.sol
+++ Modifiers.sol
@@ -3,6 +3,30 @@ pragma solidity ^0.8.7;
 
 contract Funds {
 
+ //previous task of mapping
+    constructor(address) public {
+accountOwner = msg.sender;
+
+}
+
+//mapping declaration
+   mapping(address => uint256) public balance;
+   
+    //deposit (uint256 amount) this function accepts one argument and it saves the amount a user is depositing into a mapping,
+function deposit(uint256 amount) public payable {
+ uint256 amount = msg.value;
+ balance[msg.sender += amount];
+ 
+}
+
+    //checkBalance() this function searches for the user balance inside the balance mapping and returns the balance of whoever is calling the contract.
+function checkBalance() public external returns(uint256) {
+    uint256 userBalance = balance[msg.sender];
+    return userBalance;
+}
+
+
+}
     constructor() {
     address payable owner = msg.sender;
 
